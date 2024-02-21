69f846b298f4163d7c9917a625aee160f15434ab
mqkhan <qasim_ajk@yahoo.com>
2024-02-21 10:49:48 +0100

    css added

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import News from "./componenets/news";
 export default function Home() {
   return (
     <>
-      <h1>Chas News</h1>
+      <h1 className="bg-gray-600 p-4 text-gray-200 mb-0">Chas News</h1>
       <Layout>
         <News category="pizza" />
       </Layout>
